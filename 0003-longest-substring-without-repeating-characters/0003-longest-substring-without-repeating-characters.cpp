2677c819005664385263d68568585d208d22cbe7
Arya23D <bbinoyarya244@gmail.com>
2025-06-25 19:04:05 +0530

    Time: 1 ms (88.17%), Space: 11.4 MB (86.6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,25 +3,26 @@ public:
     int lengthOfLongestSubstring(string s) {
         int l=0;
         int r=0;
-        int n= s.length();
-        vector<int> hash(256,-1);
         int maxlen=0;
+        int n = s.length();
+        vector<int>hash(256,-1);
+        int len;
 
         while(r<n)
-         {
+        {
             if(hash[s[r]]!=-1)
             {
                 if(hash[s[r]]>=l)
                 {
-                    l=hash[s[r]]+1 ;
+                  l=hash[s[r]]+1;  
                 }
             }
-            int leng = r-l+1;
-            maxlen=max(leng,maxlen);
+            len = r-l+1;
+            maxlen=max(maxlen,len);
             hash[s[r]]=r;
             r++;
-         }
-         return maxlen;
+        }
+        return maxlen;
         
     }
 };
