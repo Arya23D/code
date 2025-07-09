5628f4664d0dc7033f6b48ad1eb1cfd8997b7f9a
Arya23D <bbinoyarya244@gmail.com>
2025-07-09 11:51:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0||n==1) // intializing the base case that if n = 0 it will remain to that stair only and if n= 1 it can only step  i.e 1
+        if(n==0 || n==1)
         {
-            return 1; // for base case always return 1
+            return 1;
         }
-        vector<int>dp(n+1); // creating array of size n+1 
-        dp[0]=dp[1]=1;   // setting base case 
 
-        for(int i=2; i<=n; i++){
-        dp[i]=dp[i-1]+dp[i-2];  // formula for summing up 
+
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i = 2 ; i<=n ; i++)
+        {
+         dp[i]= dp[i-1] + dp[i-2];
         }
-        return dp[n]; 
-
+       return dp[n]; 
     }
 };
